cb07e12dc4a532f589f5ad771a4c6bca7d8232fe
Julius Khamara <khamarajulius@gmail.com>
2022-09-20 19:14:43 +0000

    Conditional Statements in JS

---
+++ JavaScript Class/script.js
@@ -0,0 +1,28 @@
+let a = 20;
+if(a === 10) {
+    console.log("The Answer Is True");
+}
+
+else{
+    console.log("Go And Study More");
+}
+
+// for( var i = 0; i<=100; ++i) {
+//     console.log(i);
+// }
+
+// for( var i = 100; i>=1; --i) {
+//     console.log(i);
+// }
+
+// var i = 0;
+// while(i<=100){
+//     console.log(i);
+//     i++;
+// }
+
+var i = 0;
+do{
+    console.log(i);
+    i++;
+}while(i<=100);
