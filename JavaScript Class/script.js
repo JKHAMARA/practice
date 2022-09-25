496ebd16ae03b4c3c6bcfc52934f605b812e1f72
Julius Khamara <khamarajulius@gmail.com>
2022-09-25 16:28:14 +0000

    JavaScript Practice

--- JavaScript Class/script.js
+++ JavaScript Class/script.js
@@ -1,11 +1,11 @@
-let a = 20;
-if(a === 10) {
-    console.log("The Answer Is True");
-}
+// let a = 20;
+// if(a === 10) {
+//     console.log("The Answer Is True");
+// }
 
-else{
-    console.log("Go And Study More");
-}
+// else{
+//     console.log("Go And Study More");
+// }
 
 // for( var i = 0; i<=100; ++i) {
 //     console.log(i);
@@ -26,3 +26,62 @@ else{
 //     console.log(i);
 //     i++;
 // }while(i<=100);
+
+// var b = [12,20,7,8];
+// console.log(b[2]);
+
+// var b = [12,20,7,8];
+// console.log(b.lenght);
+
+// var c = [5,7,9,10,54]
+// for(var i = 0; i<c.lenght; i++){
+//     console.log(c[i])
+// }
+
+// var c = [10,3,7,6,8,5];
+// for(var i = 0; i<c.length; ++i){
+//     if(i%2 === 0){
+//         console.log(c[i])
+//     }
+// }
+
+// var array = [10,26,30,47,50];
+// var jk = array.length
+// var sum = 0;
+// for(var i = 0; i < jk; i++){
+//     sum += array[i]
+// var average = sum / jk
+// }
+// console.log("The average is: " +average)
+
+//  Functions 
+// function addition(){
+//     let l = 20;
+//     let m = 45;
+//     let n = l+m;
+//     console.log("The answer is "+n)
+// }
+// addition()
+
+// function addition(l,m){
+//     let n = l+m;
+//     console.log("The answer is "+n)
+// }
+// addition(15,70)
+
+function press(){
+    var text = document.getElementById("name").value 
+    if(text === "blue"){
+        document.getElementById("header").style.colour = "blue"
+    }
+
+    else if(text === "aquamarine"){
+        document.getElementById("header").style.colour = "aquamarine"
+    }
+
+    else(text === "purple"){
+        document.getElementById("header").style.colour = "purple"
+    }
+
+}
+press()
